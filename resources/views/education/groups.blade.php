7a3529df10f3bfc6850d3c24bcd1b664d10c7ef5
Andrey <andrei_novichihin@mail.ru>
2025-02-11 18:09:05 +0300

    fix

--- resources/views/education/groups.blade.php
+++ resources/views/education/groups.blade.php
@@ -7,7 +7,7 @@
     
 </div> --}}
 <?
-    function mb_ucfirst($str, $encoding='UTF-8')
+    function mb_ucfirst_custom($str, $encoding='UTF-8')
 	{
 		$str = mb_ereg_replace('^[\ ]+', '', $str);
 		$str = mb_strtoupper(mb_substr($str, 0, 1, $encoding), $encoding).
@@ -26,7 +26,7 @@
         <div class="bg-white py-24 ">
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
                 <div class="mx-auto max-w-2xl text-center">
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{mb_ucfirst($groupName)}}</h2>
+                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{mb_ucfirst_custom($groupName)}}</h2>
                     {{-- <p class="mt-2 text-lg leading-8 text-gray-600">Тут можно описание</p> --}}
                 </div>
                 <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
