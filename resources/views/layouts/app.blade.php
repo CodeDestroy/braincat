b6927abbe3db200758a1a564e9bfb1a39fc2723a
George Astakhov <george75@list.ru>
2025-05-12 07:41:24 +0300

    add social media links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,10 +31,45 @@
         <header class="bg-white sticky top-0 z-10 shadow-[rgba(0,0,15,0.1)_0px_8px_2px_-2px]">
             <nav class="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8" aria-label="Global">
                 {{-- Логотип --}}
-                <a href="{{ url('/') }}" class="-m-1.5 p-1.5 focus:outline focus:outline-offset-0 focus:outline-mona-lisa-600 focus-visible:outline focus-visible:outline-offset-0 focus-visible:outline-mona-lisa-600">
+                <a href="{{ url('/') }}" class="m-1.5 p-1.5 border-transparent focus:border-transparent focus:ring-0">
                     <span class="sr-only">Ученый кот</span>
                     <img class="h-12 sm:h-14 w-auto" src="{{ asset('img/braincat_logo.svg') }}" alt="">
                 </a>
+                <div class="flex">
+                    <a href="https://vk.com/brain_cat" class="hidden sm:block px-1 pt-2.5 border-transparent focus:border-transparent focus:ring-0">
+                    <span class="[&>svg]:h-7 [&>svg]:w-7">
+                        <svg
+                            class="fill-salt-700 hover:fill-salt-900"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512">
+                            <path
+                            d="M31.5 63.5C0 95 0 145.7 0 247V265C0 366.3 0 417 31.5 448.5C63 480 113.7 480 215 480H233C334.3 480 385 480 416.5 448.5C448 417 448 366.3 448 265V247C448 145.7 448 95 416.5 63.5C385 32 334.3 32 233 32H215C113.7 32 63 32 31.5 63.5zM75.6 168.3H126.7C128.4 253.8 166.1 290 196 297.4V168.3H244.2V242C273.7 238.8 304.6 205.2 315.1 168.3H363.3C359.3 187.4 351.5 205.6 340.2 221.6C328.9 237.6 314.5 251.1 297.7 261.2C316.4 270.5 332.9 283.6 346.1 299.8C359.4 315.9 369 334.6 374.5 354.7H321.4C316.6 337.3 306.6 321.6 292.9 309.8C279.1 297.9 262.2 290.4 244.2 288.1V354.7H238.4C136.3 354.7 78 284.7 75.6 168.3z" />
+                        </svg>
+                        </span>
+                    </a>
+                    <a href="https://t.me/+Rvqt2hkAZKMwZDFi" class="hidden sm:block px-1 pt-2.5 border-transparent focus:border-transparent focus:ring-0">
+                    <span class="[&>svg]:h-7 [&>svg]:w-7">
+                        <svg
+                            class="fill-salt-700 hover:fill-salt-900"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 496 512">
+                            <path
+                            d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
+                        </svg>
+                        </span>
+                    </a>
+                    <a href="https://chat.whatsapp.com/KeVp4ukYJCN564y8Z1fsOg" class="hidden sm:block px-1 pt-2.5 border-transparent focus:border-transparent focus:ring-0">
+                    <span class="[&>svg]:h-7 [&>svg]:w-7">
+                        <svg
+                            class="fill-salt-700 hover:fill-salt-900"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512">
+                            <path
+                            d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                        </svg>
+                        </span>
+                    </a>
+                </div>        
                 {{-- Кнопка открытия моб меню --}}
                 <div class="flex lg:hidden">
                     <button {{-- x-on:click="expanded = !expanded" --}} onclick="document.getElementById('mobileMenu').style.display = 'block'" type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
@@ -207,7 +242,10 @@
                                 {{-- <a href="{{ url('/about') }}" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">О нас</a>--}}
                                 <a href="{{ route('education.index') }}" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Обучение</a>
                                 <a href="{{ url('/contacts') }}" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Контакты</a>
-                                <a href="{{ route('documents') }}" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Документы</a> 
+                                <a href="{{ route('documents') }}" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Документы</a>
+                                <a href="https://vk.com/brain_cat" class="-mx-3 block sm:hidden rounded-lg px-3 py-2 text-base font-normal leading-7 text-gray-900 hover:bg-gray-50">ВКонтакте</a>
+                                <a href="https://t.me/+Rvqt2hkAZKMwZDFi" class="-mx-3 block sm:hidden rounded-lg px-3 py-2 text-base font-normal leading-7 text-gray-900 hover:bg-gray-50">Telegram</a> 
+                                <a href="https://chat.whatsapp.com/KeVp4ukYJCN564y8Z1fsOg" class="-mx-3 block sm:hidden rounded-lg px-3 py-2 text-base font-normal leading-7 text-gray-900 hover:bg-gray-50">WhatsApp</a>   
                             </div>
                             @guest
                                 @if (Route::has('login'))
