ab35145a546abb6705effe25c42b736f8b33f5a8
Andrey <andrei_novichihin@mail.ru>
2025-07-09 14:51:42 +0300

    commit

--- resources/js/components/SimpleBogutenko22052025.vue
+++ resources/js/components/SimpleBogutenko22052025.vue
@@ -6,10 +6,10 @@
             <h3 class="text-2xl font-semibold leading-6 text-mona-lisa-600">Интегративный подход в нейропсихологии. Понятие и значимость</h3>
             <div class="mt-4 max-w-xl text-normal text-salt-700">
               <p>22.05.2025 с 18-00 до 19-30</p>
-              <p>Участвовать в бесплатном вебинаре</p>
+              <p>ПРосмотреть видеозапись бесплатного вебинара</p>
             </div>
             <div class="mt-5">
-              <a href="/payment/free1/20/100/0" class="inline-flex items-center rounded-md bg-mona-lisa-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-mona-lisa-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mona-lisa-500">Регистрация</a>
+              <a href="/payment/free1/20/100/0" class="inline-flex items-center rounded-md bg-mona-lisa-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-mona-lisa-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mona-lisa-500">Просмотреть</a>
             </div>
           </div>
         </div>
