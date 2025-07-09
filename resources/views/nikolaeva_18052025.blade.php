60d55557ee14ae8088a446b23dbf7b441ccabec9
Andrey <andrei_novichihin@mail.ru>
2025-07-09 14:48:53 +0300

    commit

--- resources/views/nikolaeva_18052025.blade.php
+++ resources/views/nikolaeva_18052025.blade.php
@@ -13,6 +13,7 @@
               <h1 class="mt-24 text-3xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">Что нужно знать о леворукости</h1>
               <p class="mt-6 text-lg leading-6 text-gray-600">доктор биологических наук, профессор Елена Ивановна Николаева</p>
               <div class="mt-10 flex items-center gap-x-6">
+                  <a href="#price-webinar" class="rounded-md bg-mona-lisa-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-mona-lisa-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mona-lisa-800">Приобрести запись</a>
                   <a href="#with-large-avatar" class="text-sm font-semibold leading-6 text-gray-900">Узнать больше...</a>
               </div>
           </div>
