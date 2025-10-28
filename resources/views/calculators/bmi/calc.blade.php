2c053394fa04499b0710f1324b85e8a323a9df4b
CodeDestroy <andrei_novichihin@mail.ru>
2025-10-29 01:27:36 +0300

    commit

--- resources/views/calculators/bmi/calc.blade.php
+++ resources/views/calculators/bmi/calc.blade.php
@@ -1,28 +1,24 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Anthropometry Calculator</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.0/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100 flex justify-center items-center h-screen">
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-gray-100 flex justify-center items-center h-screen">
     <form method="POST" action="{{ route('calc.calculate') }}" class="bg-white p-8 rounded-2xl shadow-md w-96 space-y-4">
         @csrf
-        <h2 class="text-xl font-semibold text-center mb-4">Anthropometry Calculator</h2>
+        <h2 class="text-xl font-semibold text-center mb-4">Калькулятор антропометрии</h2>
 
         <div>
             <label class="block font-medium">Дата рождения:</label>
-            <input type="date" name="birth_date" required class="w-full border rounded p-2">
+            <input type="date" name="birth_date" required class="block w-full rounded-md border-0 outline-none accent-mona-lisa-600 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mona-lisa-600 sm:text-sm sm:leading-6 ">
         </div>
 
         <div>
             <label class="block font-medium">Дата вычисления:</label>
-            <input type="date" name="calc_date" value="<?php echo date('Y-m-d'); ?>" required class="w-full border rounded p-2">
+            <input type="date" name="calc_date" value="<?php echo date('Y-m-d'); ?>" required class="block w-full rounded-md border-0 outline-none accent-mona-lisa-600 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mona-lisa-600 sm:text-sm sm:leading-6 ">
         </div>
 
         <div>
             <label class="block font-medium">Пол:</label>
-            <select name="sex" class="w-full border rounded p-2">
+            <select name="sex" class="block w-full rounded-md border-0 outline-none accent-mona-lisa-600 py-2 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mona-lisa-600 sm:text-sm sm:leading-6 ">
                 <option value="male">Мужской</option>
                 <option value="female">Женский</option>
             </select>
@@ -30,17 +26,17 @@
 
         <div>
             <label class="block font-medium">Рост (см):</label>
-            <input type="number" step="0.1" name="height" required class="w-full border rounded p-2">
+            <input type="number" step="0.1" name="height" required class="block w-full rounded-md border-0 outline-none accent-mona-lisa-600 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mona-lisa-600 sm:text-sm sm:leading-6 ">
         </div>
 
         <div>
             <label class="block font-medium">Вес (кг):</label>
-            <input type="number" step="0.1" name="weight" required class="w-full border rounded p-2">
+            <input type="number" step="0.1" name="weight" required class="block w-full rounded-md border-0 outline-none accent-mona-lisa-600 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mona-lisa-600 sm:text-sm sm:leading-6 ">
         </div>
 
-        <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2">
+        <button type="submit" class="w-full bg-mona-lisa-600 hover:bg-mona-lisa-700 text-white rounded-lg py-2">
             Рассчитать
         </button>
     </form>
-</body>
-</html>
+</div>
+@endsection
--- resources/views/calculators/bmi/result.blade.php
+++ resources/views/calculators/bmi/result.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Результаты расчёта</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.0/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100 flex justify-center items-center h-screen">
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-gray-100 flex justify-center items-center h-screen">
     <div class="bg-white p-8 rounded-2xl shadow-md w-96 space-y-3">
         <h2 class="text-xl font-semibold text-center mb-4">Результаты</h2>
 
@@ -15,9 +11,9 @@
         <p><strong>BMI:</strong> {{ $bmi }}</p>
         <p><strong>BMI SDS (for Height age):</strong> {{ $bmiSDS }}</p>
 
-        <a href="{{ route('calc.form') }}" class="block mt-4 text-center bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2">
+        <a href="{{ route('calc.form') }}" class="block mt-4 text-center bg-mona-lisa-600 hover:bg-mona-lisa-700 text-white rounded-lg py-2">
             Новый расчёт
         </a>
     </div>
-</body>
-</html>
+</div>
+@endsection
